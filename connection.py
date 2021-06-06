9721062f8b51d8600cf1618e1ddabbbfa2bc52b8
naveeng143 <45759489+naveeng143@users.noreply.github.com>
2021-06-06 18:58:38 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,10 +1,16 @@
 from getpass import getpass
+from os import close
 from mysql.connector import connect, Error
-
 
 try:
     with connect(host="localhost", user="root", password="Lodestone#123") as connection:
-        print(connection)
-
-except Exception as e:
+        create_db_query = "CREATE DATABASE online_movie_rating"
+        connection.reconnect()
+    with connection.cursor() as cursor:
+        cursor.execute(create_db_query)
+        
+except Error as e:
     print(e)
+    print(connection)
+finally:
+    connection.close()
